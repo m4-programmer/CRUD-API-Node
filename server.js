f0f2262fec0b8fbefe2f2633444ce9944c1ddc1b
Adolphus Miracle <miraboy13@gmail.com>
2024-01-21 22:01:21 +0100

    feat: implemented post request to post products

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const mongodbPassword = require('./password')
+const Product = require('./models/productModels')
 const app = express()
 
 
 //middleware
-// app.use(express.json)
+app.use(express.json())
 
+//routes
+app.post('/post', async(req, res)=>{
+    try {
+        const product = await Product.create(req.body)
+        res.status(200).send(product)
+
+    } catch (error) {   
+        console.log(error.message)
+        res.status(500).json({message: error.message})
+    }
+})
 
 mongoose.connect("mongodb+srv://sloovi:"+ mongodbPassword.MongoPassword+"@template.t6v0y.mongodb.net/Node-API?retryWrites=true&w=majority")
 .then(()=>{
