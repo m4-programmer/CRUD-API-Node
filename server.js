3095a5263640a4c1cd4db37f9c7dc17936a15906
Adolphus Miracle <miraboy13@gmail.com>
2024-01-21 22:11:09 +0100

    feat: created method to display success json response

--- server.js
+++ server.js
@@ -8,11 +8,20 @@ const app = express()
 //middleware
 app.use(express.json())
 
+const success = (message='', data = [], status =200) => {
+    return {
+        message,
+        data,
+        status
+    }
+}
+
+
 //routes
 app.post('/post', async(req, res)=>{
     try {
         const product = await Product.create(req.body)
-        res.status(200).send(product)
+        res.status(200).send(success("product created successfully",product))
 
     } catch (error) {   
         console.log(error.message)
