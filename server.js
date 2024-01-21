b024815d732ad6e8a55a0079f1352cb22cc20a62
Adolphus Miracle <miraboy13@gmail.com>
2024-01-21 21:42:37 +0100

    feat: added mongodb and set up the connection

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const mongodbPassword = require('./password')
 const app = express()
 
 
-//routes
+//middleware
+// app.use(express.json)
 
 
-app.get('/', (req, res) => {
-    
+mongoose.connect("mongodb+srv://sloovi:"+ mongodbPassword.MongoPassword+"@template.t6v0y.mongodb.net/Node-API?retryWrites=true&w=majority")
+.then(()=>{
+    app.listen(3000, () => console.log('listening on port 3000'))
+    console.log('Connected to MongoDB')
 })
-
-app.listen(3000, () => console.log('listening on port 3000'))
+.catch((e)=>{
+    console.log(e)
+})
