e3ac60784092bcd0b8668c5c144acd090647bdbc
Adolphus Miracle <miraboy13@gmail.com>
2024-01-22 10:38:06 +0100

    refactor: refactored all controller logic to controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,5 @@
 const {success, error} = require('../helpers/apiHelper')
+const Product = require('../models/productModels')
 
 const getProduct = async(req, res) => {
     try {
@@ -10,7 +11,65 @@ const getProduct = async(req, res) => {
     }
 }
 
+const postProduct = async(req, res)=>{
+    try {
+        const product = await Product.create(req.body)
+        res.status(200).send(success("product created successfully",product))
+
+    } catch (err) {   
+        console.log(err.message)
+        res.status(500).json(error(err.message))
+    }
+};
+
+const getSingleProduct = async(req, res)=>{
+    try {
+        const {id} = req.params
+        const product = await Product.findById(id)
+        res.send(success("product retrieved successfully", product))
+
+    } catch (err) {   
+        console.log(err.message)
+        res.status(500).json(error(err.message))
+    }
+}
+
+const updateProduct = async(req, res)=>{
+    try {
+        const {id} = req.params
+        const product = await Product.findByIdAndUpdate(id, req.body);
+        if (!product) {
+            res.status(404).json(error("product not found", [], 404))
+        }
+        const updatedProduct = await Product.findById(id)
+        res.send(success("product updated successfully", updatedProduct))
+
+    } catch (err) {   
+        console.log(err.message)
+        res.status(500).json(error(err.message))
+    }
+}
+
+const deleteProduct = async(req, res)=>{
+    try {
+        const {id} = req.params
+        const product = await Product.findByIdAndDelete(id);
+        if (!product) {
+            res.status(404).json(error("product not found", [], 404))
+        }
+        // const updatedProduct = await Product.findById(id)
+        res.send(success("product deleted successfully"))
+
+    } catch (err) {   
+        console.log(err.message)
+        res.status(500).json(error(err.message))
+    }
+}
 
 module.exports = {
-    getProduct
+    getProduct,
+    postProduct,
+    getSingleProduct,
+    updateProduct,
+    deleteProduct
 }
--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const mongodbPassword = require('./password')
-const Product = require('./models/productModels')
-const {getProduct, } = require('./controllers/productController')
+const {getProduct, deleteProduct, updateProduct, getSingleProduct, postProduct, } = require('./controllers/productController')
 const app = express()
 
 
 //middleware
 app.use(express.json())
-// 65ae2bebe3af0438551651cd
+
 
 
 app.get('/', (req, res)=>{
@@ -17,60 +16,13 @@ app.get('/', (req, res)=>{
 //routes
 app.get('/product', getProduct)
 
-app.post('/product', async(req, res)=>{
-    try {
-        const product = await Product.create(req.body)
-        res.status(200).send(success("product created successfully",product))
+app.post('/product', postProduct)
 
-    } catch (err) {   
-        console.log(err.message)
-        res.status(500).json(error(err.message))
-    }
-})
+app.get('/product/:id', getSingleProduct)
 
-app.get('/product/:id', async(req, res)=>{
-    try {
-        const {id} = req.params
-        const product = await Product.findById(id)
-        res.send(success("product retrieved successfully", product))
+app.put('/product/:id', updateProduct)
 
-    } catch (err) {   
-        console.log(err.message)
-        res.status(500).json(error(err.message))
-    }
-})
-
-app.put('/product/:id', async(req, res)=>{
-    try {
-        const {id} = req.params
-        const product = await Product.findByIdAndUpdate(id, req.body);
-        if (!product) {
-            res.status(404).json(error("product not found", [], 404))
-        }
-        const updatedProduct = await Product.findById(id)
-        res.send(success("product updated successfully", updatedProduct))
-
-    } catch (err) {   
-        console.log(err.message)
-        res.status(500).json(error(err.message))
-    }
-})
-
-app.delete('/product/:id', async(req, res)=>{
-    try {
-        const {id} = req.params
-        const product = await Product.findByIdAndDelete(id);
-        if (!product) {
-            res.status(404).json(error("product not found", [], 404))
-        }
-        // const updatedProduct = await Product.findById(id)
-        res.send(success("product deleted successfully"))
-
-    } catch (err) {   
-        console.log(err.message)
-        res.status(500).json(error(err.message))
-    }
-})
+app.delete('/product/:id', deleteProduct)
 
 
 
