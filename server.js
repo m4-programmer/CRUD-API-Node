d323fc29bebb6958c7b8b534687993efaa02cc0f
Adolphus Miracle <miraboy13@gmail.com>
2024-01-22 09:52:04 +0100

    feat: implemented logic to fetch products from db

--- server.js
+++ server.js
@@ -28,6 +28,16 @@ app.get('/', (req, res)=>{
     res.send(success("Hello Working"))
 })
 //routes
+app.get('/product', async(req, res)=>{
+    try {
+        const products = await Product.find({});
+        res.status(200).send(success("product retrieved successfully",products))
+    } catch (e) {
+        console.log(e.message)
+        res.status(500).json(error(e.message))        
+    }
+})
+
 app.post('/product', async(req, res)=>{
     try {
         const product = await Product.create(req.body)
