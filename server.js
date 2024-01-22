929383ab156f399f3f24b33346b47a26591eefe3
Adolphus Miracle <miraboy13@gmail.com>
2024-01-22 10:32:30 +0100

    refactor: refactored get Product

--- server.js
+++ server.js
@@ -2,41 +2,20 @@ const express = require('express')
 const mongoose = require('mongoose')
 const mongodbPassword = require('./password')
 const Product = require('./models/productModels')
+const {getProduct, } = require('./controllers/productController')
 const app = express()
 
 
 //middleware
 app.use(express.json())
+// 65ae2bebe3af0438551651cd
 
-const success = (message='', data = [], status =200) => {
-    return {
-        message,
-        data,
-        status
-    }
-}
-
-const error = (message, error=[], status =500) =>{
-    return {
-        message,
-        error,
-        status
-    }
-}
 
 app.get('/', (req, res)=>{
     res.send(success("Hello Working"))
 })
 //routes
-app.get('/product', async(req, res)=>{
-    try {
-        const products = await Product.find({});
-        res.status(200).send(success("product retrieved successfully",products))
-    } catch (e) {
-        console.log(e.message)
-        res.status(500).json(error(e.message))        
-    }
-})
+app.get('/product', getProduct)
 
 app.post('/product', async(req, res)=>{
     try {
